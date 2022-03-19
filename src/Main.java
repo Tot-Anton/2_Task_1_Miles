c50fd8652e1ac0a6c95560151efa30c7c8f3887c
Anton Belmisov <antonbelmisov@mail.ru>
2022-03-19 14:27:25 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 5_000;
+        int bonus = 20;
+
+        System.out.println("Бонусных милей вышло:");
+        System.out.println((amount / bonus) + "миля");
+
+    }
+}
